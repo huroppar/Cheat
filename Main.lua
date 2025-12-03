a97aafdeaba9ece7cbf0a5e4987fac6414914f2b
huroppar <70945063+huroppar@users.noreply.github.com>
2025-12-04 02:46:03 +0900

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/huroppar/Cheat/refs/heads/main/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/huroppar/Cheat/refs/heads/main/Load.lua"))()
