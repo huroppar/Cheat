a2084f0b102da6bc9c59dd7a230a7b79aa481ed1
huroppar <70945063+huroppar@users.noreply.github.com>
2025-12-12 00:31:36 +0900

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,5 +1,5 @@
 -- OrionLibロード
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/WRUyYTdY"))()
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
